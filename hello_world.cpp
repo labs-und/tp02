141839c4f619a530bcf2edca535d7f692a2c9a84
undefinedev <87879854+undefinedev@users.noreply.github.com>
2022-03-09 01:42:57 +0300

    Conflict adding

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,8 +4,8 @@
 int main() {
 	std::cout << "input your name" << std::endl;
 	std::string name;
-	std::cin >> name;    //request name
-	std::cout << "HELLO WORlD FROM " << name << std::endl;   //output
+	std::cin >> name;    //Request name
+	std::cout << "HELLO WORlD FROM " << name << std::endl;   //Output console
 	return 0;
 }
 
