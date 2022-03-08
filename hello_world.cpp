1d9605aedfc062edb28dbb744b4fb35b6fd642f7
unefinedev <undefined_dev@inbox.ru>
2022-03-08 23:45:15 +0300

    comms added

--- hello_world.cpp
+++ hello_world.cpp
@@ -4,8 +4,8 @@
 int main() {
 	std::cout << "input your name" << std::endl;
 	std::string name;
-	std::cin >> name;
-	std::cout << "HELLO WORlD FROM " << name << std::endl;
+	std::cin >> name;    //request name
+	std::cout << "HELLO WORlD FROM " << name << std::endl;   //output
 	return 0;
 }
 
